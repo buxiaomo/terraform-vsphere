40e84a29faadd0e17dc7d45845907418e7f5255c
buxiaomo <95112082@qq.com>
2022-03-10 15:08:06 +0800

    update

---
+++ main.tf
@@ -0,0 +1,119 @@
+#===============================================================================
+# vSphere Backend
+#===============================================================================
+terraform {
+  backend "s3" {
+    region                      = "minio"
+    force_path_style            = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    # skip_requesting_account_id  = true
+    # skip_get_ec2_platforms      = true
+  }
+}
+
+
+#===============================================================================
+# vSphere Provider
+#===============================================================================
+provider "vsphere" {
+  user                 = var.vsphere_user
+  password             = var.vsphere_password
+  vsphere_server       = var.vsphere_server
+  allow_unverified_ssl = true
+}
+
+
+#===============================================================================
+# vSphere Data
+#===============================================================================
+data "vsphere_datacenter" "datacenter" {
+  name = var.vsphere_datacenter
+}
+
+data "vsphere_compute_cluster" "compute_cluster" {
+  name          = var.vsphere_compute_cluster
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_datastore_cluster" "vsphere_datastore_cluster" {
+  name          = var.vsphere_datastore_cluster
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = var.vsphere_virtual_machine_template
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_network" "network" {
+  name          = var.vsphere_network
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+
+#===============================================================================
+# vSphere Resources
+#===============================================================================
+resource "vsphere_resource_pool" "resource_pool" {
+  name                    = var.project_name
+  parent_resource_pool_id = data.vsphere_compute_cluster.compute_cluster.resource_pool_id
+}
+
+resource "vsphere_virtual_machine" "kubernetes" {
+  for_each             = var.vms
+  name                 = each.value["hostname"]
+  resource_pool_id     = vsphere_resource_pool.resource_pool.id
+  datastore_cluster_id = data.vsphere_datastore_cluster.vsphere_datastore_cluster.id
+  num_cpus             = each.value["cpu"]
+  memory               = each.value["memory"]
+  firmware             = data.vsphere_virtual_machine.template.firmware
+  guest_id             = data.vsphere_virtual_machine.template.guest_id
+  scsi_type            = data.vsphere_virtual_machine.template.scsi_type
+
+  network_interface {
+    network_id   = data.vsphere_network.network.id
+    adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
+  }
+
+  disk {
+    label            = "disk0"
+    size             = data.vsphere_virtual_machine.template.disks.0.size
+    eagerly_scrub    = data.vsphere_virtual_machine.template.disks.0.eagerly_scrub
+    thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
+  }
+
+  disk {
+    label       = "disk1"
+    size        = "50"
+    unit_number = 1
+    thin_provisioned = true
+  }
+
+  disk {
+    label       = "disk2"
+    size        = "50"
+    unit_number = 2
+    thin_provisioned = true
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+
+    customize {
+      linux_options {
+        domain    = ""
+        host_name = each.value["hostname"]
+      }
+
+      ipv4_gateway    = var.gateway
+      dns_server_list = var.dns
+
+      network_interface {
+        ipv4_address = each.value["ipaddr"]
+        ipv4_netmask = var.ipv4_netmask
+      }
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,69 @@
+# ------------------- vSphere -------------------
+variable "vsphere_server" {
+  description = "vsphere server url"
+  type        = string
+}
+
+variable "vsphere_user" {
+  description = "login vsphere server username"
+  type        = string
+}
+
+variable "vsphere_password" {
+  description = "login vsphere server password"
+  type        = string
+}
+
+variable "vsphere_datacenter" {
+  description = "vsphere datacenter name"
+  type        = string
+}
+
+variable "vsphere_network" {
+  description = "virtual machine network name"
+  type        = string
+  default     = "VM Network"
+}
+
+variable "vsphere_datastore_cluster" {
+  description = "datastore cluster name"
+  type        = string
+}
+
+# -----------------------------------------------
+
+# --------------- Template Setup ----------------
+variable "vsphere_virtual_machine_template" {
+  type = string
+}
+
+variable "project_name" {
+  type = string
+}
+
+
+variable "dns" {
+  type = list(string)
+}
+
+variable "vms" {
+  type = map(object({
+    hostname = string
+    ipaddr   = string
+    cpu      = number
+    memory   = number
+  }))
+}
+
+variable "ipv4_netmask" {
+  type = string
+}
+
+variable "gateway" {
+  type = string
+}
+
+
+variable "vsphere_compute_cluster" {
+  type = string
+}
